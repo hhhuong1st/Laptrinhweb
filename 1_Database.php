f61e42796811a2e1679e193ac7b02c7cdaf0003b
huong <huynhhuyenhuong2006@gmail.com>
2026-01-28 16:28:33 +0700

    update

--- 1_Database.php
+++ 1_Database.php
@@ -1,18 +1,14 @@
 <?php
-// Khai báo thông tin máy chủ
 $servername = "localhost";
 $username = "root";
 $password = ""; 
 
-// Kết nối đến MySQL 
 $conn = new mysqli($servername, $username, $password);
 
-// Kiểm tra xem kết nối có lỗi không
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
-
-// Viết lệnh tạo Database
+// Tạo Database
 $sql = "CREATE DATABASE quanlybanhang";
 
 if ($conn->query($sql) === TRUE) {
